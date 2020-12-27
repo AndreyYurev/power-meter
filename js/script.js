3126050f5e5b282bd78e6f5f40ce31f004561f18
AndreyYurev <65871877+AndreyYurev@users.noreply.github.com>
2020-12-27 11:15:28 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ let scaleBarAnimation = anime({
 });
 
 // Назначение начала анимации при нажатии на кнопку
-document.querySelector('.new-game__button').onclick = scaleHorLineAnimation.play;
-document.querySelector('.start__new-game').onclick = scaleBarAnimation.play;
+document.querySelector('.start__new-game').onclick = scaleHorLineAnimation.play;
+document.querySelector('.new-game__wrapper').onclick = scaleBarAnimation.play;
 
 
 
@@ -34,18 +34,21 @@ let paragraphStart = document.querySelector('.text-block__paragraph--start');
 let paragraphHit = document.querySelector('.text-block__paragraph--hit');
 let startButton = document.querySelector('.new-game__button');
 let hitButton = document.querySelector('.hit__button');
+let restartButton = document.querySelector('.restart__button');
 let hammer = document.querySelector('.hammer__hammer-image');
 
 // Функция запуска игры
 const startGame = () => {
-	paragraphStart.classList.add('paragraph--hidden');
-	paragraphHit.classList.remove('paragraph--hidden');
-	startButton.classList.add('button--hidden');
-	hitButton.classList.remove('button--hidden');
+	paragraphStart.classList.add('hidden');
+	paragraphHit.classList.remove('hidden');
+	startButton.classList.add('hidden');
+	hitButton.classList.remove('hidden');
 	hammer.classList.add('hammer__hammer-0deg');
 };
 startButton.addEventListener('click', e => {
 	startGame();
+	// запускаем отслеживание изменения высоты scale__element
+	resize_ob.observe(document.querySelector(".scale__element"));
 });
 
 
@@ -64,7 +67,92 @@ let measureScale8 = document.querySelector('.scale-8');
 let measureScale9 = document.querySelector('.scale-9');
 let scaleBar = document.querySelector('.scale__element');
 let scaleBarHeight
-
+let redButton = document.querySelector('.button__button-image');
+let redButtonActive = document.querySelector('.button__button-image--active');
+let glowLayer = document.querySelector('.glow__layer-glow');
+let under100 = document.querySelector('.text-block__paragraph--under100');
+let under130 = document.querySelector('.text-block__paragraph--under130');
+let hit140 = document.querySelector('.text-block__paragraph--hit140');
+let robot1 = document.querySelector('.robot__image-1');
+let robot2 = document.querySelector('.robot__image-2');
+let robot3 = document.querySelector('.robot__image-3');
+/* При определенных значениях scaleBarHeight добавляем
+класс visible у нужного слоя основной шкалы */
+let changeMeasure = function () {
+	if (scaleBarHeight < 20) {
+		measureScale2.classList.add('visible');
+		under100.classList.remove('hidden');
+		under130.classList.add('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 20 && scaleBarHeight < 40) {
+		measureScale3.classList.add('visible');
+		under100.classList.remove('hidden');
+		under130.classList.add('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 40 && scaleBarHeight < 60) {
+		measureScale4.classList.add('visible');
+		under100.classList.remove('hidden');
+		under130.classList.add('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 60 && scaleBarHeight < 80) {
+		measureScale5.classList.add('visible');
+		under100.classList.remove('hidden');
+		under130.classList.add('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 80 && scaleBarHeight < 100) {
+		measureScale6.classList.add('visible');
+		under100.classList.add('hidden');
+		under130.classList.remove('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 100 && scaleBarHeight < 115) {
+		measureScale7.classList.add('visible');
+		under100.classList.add('hidden');
+		under130.classList.remove('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 115 && scaleBarHeight < 130) {
+		measureScale8.classList.add('visible');
+		under100.classList.add('hidden');
+		under130.classList.remove('hidden');
+		hit140.classList.add('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.remove('hidden');
+		robot3.classList.add('hidden');
+	};
+	if (scaleBarHeight >= 130 && scaleBarHeight <= 140) {
+		measureScale9.classList.add('visible');
+		glowLayer.classList.add('visible');
+		under100.classList.add('hidden');
+		under130.classList.add('hidden');
+		hit140.classList.remove('hidden');
+		robot1.classList.add('hidden');
+		robot2.classList.add('hidden');
+		robot3.classList.remove('hidden');
+	};
+};
 // Функция отслеживания изменения высоты scale__element
 let resize_ob = new ResizeObserver(function (entries) {
 	let rect = entries[0].contentRect;
@@ -72,49 +160,61 @@ let resize_ob = new ResizeObserver(function (entries) {
 	return scaleBarHeight = height;
 });
 
-// запускаем отслеживание изменения высоты scale__element
-resize_ob.observe(document.querySelector(".scale__element"));
 
 // При нажатии на кнопку "УДАР!":
 hitButton.addEventListener('click', e => {
 
 	// останавливаем анимацию шкалы
-	document.querySelector('.new-game__wrapper').onclick = scaleHorLineAnimation.pause;
-	document.querySelector('.start__new-game').onclick = scaleBarAnimation.pause;
+	document.querySelector('.start__new-game').onclick = scaleHorLineAnimation.pause;
+	document.querySelector('.hit__wrapper').onclick = scaleBarAnimation.pause;
 
 	// останавливаем отслеживание изменения высоты scale__element
 	resize_ob.unobserve(document.querySelector(".scale__element"));
 
 	// округляем scaleBarHeight
 	console.log(Math.floor(scaleBarHeight));
+	hammer.classList.add('hammer__hammer-striker');
 
 	/* При определенных значениях scaleBarHeight удаляется
-	класс scale--hidden у нужного слоя основной шкалы */
+	класс transparent у нужного слоя основной шкалы */
+	changeMeasure();
+	hitButton.classList.add('hidden');
+	paragraphHit.classList.add('hidden');
+	redButton.classList.add('hidden');
+	redButtonActive.classList.remove('hidden');
+	restartButton.classList.remove('hidden');
 
-	if (scaleBarHeight < 20) {
-		measureScale2.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 20 && scaleBarHeight < 40) {
-		measureScale3.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 40 && scaleBarHeight < 60) {
-		measureScale4.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 60 && scaleBarHeight < 80) {
-		measureScale5.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 80 && scaleBarHeight < 100) {
-		measureScale6.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 100 && scaleBarHeight < 115) {
-		measureScale7.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 115 && scaleBarHeight < 130) {
-		measureScale8.classList.remove('scale--hidden');
-	};
-	if (scaleBarHeight >= 130 && scaleBarHeight < 140) {
-		measureScale9.classList.remove('scale--hidden');
-	};
 });
 
+restartButton.addEventListener('click', e => {
+	hammer.classList.remove('hammer__hammer-striker');
+	document.querySelector('.start__new-game').onclick = scaleHorLineAnimation.play;
+	document.querySelector('.restart__wrapper').onclick = scaleBarAnimation.play;
+	glowLayer.classList.remove('visible');
+	restartButton.classList.add('hidden');
 
+	measureScale2.classList.remove('visible');
+	measureScale3.classList.remove('visible');
+	measureScale4.classList.remove('visible');
+	measureScale5.classList.remove('visible');
+	measureScale6.classList.remove('visible');
+	measureScale7.classList.remove('visible');
+	measureScale8.classList.remove('visible');
+	measureScale9.classList.remove('visible');
+
+	robot1.classList.remove('hidden');
+	robot2.classList.add('hidden');
+	robot3.classList.add('hidden');
+
+	resize_ob.observe(document.querySelector(".scale__element"));
+
+	under100.classList.add('hidden');
+	under130.classList.add('hidden');
+	hit140.classList.add('hidden');
+	hitButton.classList.add('hidden');
+
+	paragraphHit.classList.remove('hidden');
+	redButton.classList.remove('hidden');
+	redButtonActive.classList.add('hidden');
+	hitButton.classList.remove('hidden');
+});
